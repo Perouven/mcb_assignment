080eb6f00cb9b2715d95af43c1215339c6d6f245
Perouven <vperouven@gmail.com>
2024-01-29 23:15:07 -0500

    Implemented MongoDB

--- components/report.tsx
+++ components/report.tsx
@@ -1,13 +1,54 @@
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+
+
 export default function Report(){
 
+    
     return(
+
+        
+
         <div>
-         <table>
-            <td></td>
-            Step 1: Select your filter
-            Step 2: Generate your report
-            Step 3: Save your Report
-            </table>
-        </div>
+                <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Country</TableCell>
+            <TableCell align="right">Country Code</TableCell>
+            <TableCell align="right">Region</TableCell>
+            <TableCell align="right">Indicator Name</TableCell>
+            <TableCell align="right">Indicator Name-1</TableCell>
+            <TableCell align="right">Changes in Scores</TableCell>
+            <TableCell align="right">Indicator name Rank</TableCell>
+            <TableCell align="right">Indicator Name Rank-1</TableCell>
+            <TableCell align="right">Changes in Rank</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+         
+            <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+              </TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+             </div>
     );
 }
