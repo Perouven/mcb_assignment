2b087c33275cd64027f21f4cd8141ab37f39d6d0
Perouven <vperouven@gmail.com>
2024-01-29 20:14:48 -0500

    initial commit

---
+++ components/report.tsx
@@ -0,0 +1,13 @@
+export default function Report(){
+
+    return(
+        <div>
+         <table>
+            <td></td>
+            Step 1: Select your filter
+            Step 2: Generate your report
+            Step 3: Save your Report
+            </table>
+        </div>
+    );
+}
