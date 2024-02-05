3be33415cd9bb8cb8694120f7a690701aa94cf45
Perouven <vperouven@gmail.com>
2024-02-04 22:00:24 -0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  
-    typescript: { ignoreBuildErrors: true }
-};
 
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+/* ...Your other config rules */
+}
+
+module.exports = nextConfig
